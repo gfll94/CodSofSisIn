4dcbdae28d2631f4f7d7443a004d7682a393287a
gfll94 <gomez.luis.2024@cbtis-246.edu.mx>
2025-02-28 19:59:52 -0600

    Add files via upload

---
+++ excepciones.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+print(esta_es_una_variables
+(escribe tu edad)
+enero 31
+dia nac = 6
+print año_actual-año_nac
+
+
+# In[7]:
+
+
+10 * (1/0)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+    10 * (1/0)
+          ~^~
+
+
+# In[29]:
+
+
+raise NameError('HiThere')
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module
+
+
+# In[31]:
+
+
+try:
+    open("database.sqlite")
+
+
+# In[33]:
+
+
+Traceback (most recent call last):
+  File "<stdin>", line 4, in <module>
+
+
+# In[35]:
+
+
+try:
+    func()
+
+
+# In[37]:
+
+
+try:
+    raise KeyboardInterrupt
+finally:
+
+
+# In[39]:
+
+
+Traceback (most recent call last):
+  File "<stdin>", line 2, in <module>
+
+
+# In[41]:
+
+
+for line in open("myfile.txt"):
+    print(line, end="")
+
+
+# In[67]:
+
+
+while True print('Hello world')
+  File "<stdin>", line 1
+    while True print('Hello world')
+
