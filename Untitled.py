16c53a6a66a9968390fc57324f9fcc1f5efe128c
gfll94 <gomez.luis.2024@cbtis-246.edu.mx>
2025-03-20 15:09:10 -0600

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+class humano:
+    pass
+class humano2(humano):
+    pass
+class humano3:
+    pass
+class humano4(humano2, humano3):
+    pass
+
